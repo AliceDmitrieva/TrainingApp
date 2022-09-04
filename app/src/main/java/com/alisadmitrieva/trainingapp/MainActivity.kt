d5aa6b11de04e35b666f6470e77fb2dc48d8671e
Alisa Dmitrieva <coolalisa@yandex.ru>
2022-09-04 14:36:33 +0300

    initial setup

---
+++ app/src/main/java/com/alisadmitrieva/trainingapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.alisadmitrieva.trainingapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.alisadmitrieva.trainingapp.ui.theme.CleanArchitectureNoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CleanArchitectureNoteAppTheme {
+                
+            }
+        }
+    }
+}
