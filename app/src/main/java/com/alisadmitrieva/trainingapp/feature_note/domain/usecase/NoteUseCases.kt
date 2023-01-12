781efbd59df3d56f3c255786d87cb7870c3bac21
Alisa Dmitrieva <coolalisa@yandex.ru>
2023-01-12 23:36:13 +0300

    delete notes code

--- app/src/main/java/com/alisadmitrieva/trainingapp/feature_note/domain/usecase/NoteUseCases.kt
+++
@@ -1,8 +0,0 @@
-package com.alisadmitrieva.trainingapp.feature_note.domain.usecase
-
-data class NoteUseCases(
-    val getNotes: GetNotes,
-    val deleteNote: DeleteNote,
-    val addNote: AddNote,
-    val getNote: GetNote,
-)
--- app/src/main/java/com/alisadmitrieva/trainingapp/feature_note/domain/utils/OrderType.kt
+++
@@ -1,6 +0,0 @@
-package com.alisadmitrieva.trainingapp.feature_note.domain.utils
-
-sealed class OrderType {
-    object Ascending: OrderType()
-    object Descending: OrderType()
-}
