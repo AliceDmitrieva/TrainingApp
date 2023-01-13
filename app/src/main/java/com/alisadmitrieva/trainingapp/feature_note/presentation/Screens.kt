17e6272a78a5e75374df90a0b15e876877656193
Alisa Dmitrieva <coolalisa@yandex.ru>
2023-01-13 23:50:01 +0300

    change screens

--- app/src/main/java/com/alisadmitrieva/trainingapp/feature_note/presentation/Screens.kt
+++ app/src/main/java/com/alisadmitrieva/trainingapp/feature_note/presentation/Screens.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,56 +13,24 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.alisadmitrieva.trainingapp.R
-import com.alisadmitrieva.trainingapp.feature_note.presentation.add_training.components.AddEditNoteScreen
-import com.alisadmitrieva.trainingapp.feature_note.presentation.notes.components.NotesScreen
-import com.alisadmitrieva.trainingapp.feature_note.presentation.util.Screen
-import com.alisadmitrieva.trainingapp.ui.theme.CleanArchitectureNoteAppTheme
 
 @Composable
 fun TrainingsScreen() {
-    CleanArchitectureNoteAppTheme {
-        Surface(
-            color = MaterialTheme.colors.background
-        ) {
-            val navController = rememberNavController()
-            NavHost(
-                navController = navController,
-                startDestination = Screen.NotesScreen.route
-            ) {
-                composable(route = Screen.NotesScreen.route) {
-                    NotesScreen(navController = navController)
-                }
-                composable(
-                    route = Screen.AddEditNoteScreen.route
-                        + "?noteId={noteId}&noteColor={noteColor}",
-                    arguments = listOf(
-                        navArgument(
-                            name = "noteId"
-                        ) {
-                            type = NavType.IntType
-                            defaultValue = -1
-                        },
-                        navArgument(
-                            name = "noteColor"
-                        ) {
-                            type = NavType.IntType
-                            defaultValue = -1
-                        },
-                    )
-                ) {
-                    val color = it.arguments?.getInt("noteColor") ?: -1
-                    AddEditNoteScreen(
-                        navController = navController,
-                        noteColor = color)
-                }
-            }
-        }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.teal_200))
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "My Trainings Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
     }
 }
 
@@ -77,7 +43,7 @@ fun NotesScreen() {
             .wrapContentSize(Alignment.Center)
     ) {
         Text(
-            text = "My Notes Screen",
+            text = "My Nutrition Screen",
             fontWeight = FontWeight.Bold,
             color = Color.White,
             modifier = Modifier.align(Alignment.CenterHorizontally),
