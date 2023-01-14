0121ba28749d6650160f8f113cd204fed2064b19
Alisa Dmitrieva <coolalisa@yandex.ru>
2023-01-14 23:48:32 +0300

    delete Screen

--- app/src/main/java/com/alisadmitrieva/trainingapp/feature_note/presentation/util/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.alisadmitrieva.trainingapp.feature_note.presentation.util
-
-sealed class Screen(val route: String) {
-    object NotesScreen: Screen("notes_screen")
-    object AddEditNoteScreen: Screen("add_edit_note_screen")
-}
